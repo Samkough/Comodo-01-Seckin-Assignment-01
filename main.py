d85a49f9826ce8782d07250552568779c0c3edc2
Sammy Samkough <sammysamkough@gmail.com>
2018-04-13 17:55:10 -0400

    Was able to have program run, but everytime I try to open the database in DB Browser, it's asking for a password which I don't have. It worked when I was running it once, but then went back to keep requesting me for a password. Even when it worked that one time it didn't show any data. Weird stuff.

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
-import csv
-import sys
 import sqlite3
+import csv
 import os
-
-# creates the main parts to connecting to a database
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-
-def create_table():
-    c.execute("CREATE TABLE IF NOT EXISTS seckin(Time INTEGER, Duration INTEGER, SrcDevice TEXT, DstDevice TEXT, Protocol INTEGER, SrcPort INTEGER, DstPort INTEGER, SrcPackets INTEGER, DstPackets INTEGER, SrcBytes INTEGER, DstBytes INTEGER)")
-create_table()
 
 script_dir = os.path.dirname(r'C:\Users\sammy\Desktop\DOWNLOADS\PROGRAMMING\Comodo\_Data-Analysis') #<-- absolute dir the script is in
 rel_path = "_Data-Analysis/netflow_day-02.csv"
 abs_file_path = os.path.join(script_dir, rel_path)
 
-with open(abs_file_path, 'rb') as fin:
-        csv_reader = csv.DictReader(fin)
-        to_db = [(i['Time'], i['Duration'], i['SrcDevice'], i['DstDevice'], i['Protocol'], i['SrcPort'], i['DstPort'], i['SrcPackets'], i['DstPackets'], i['SrcBytes'], i['DstBytes']) for i in csv_reader]
+f = open(abs_file_path,'r') # open the csv data file
+next(f, None) # skip the header row
+reader = csv.reader(f)
 
-c.executemany("INSERT INTO seckin(Time INTEGER, Duration INTEGER, SrcDevice TEXT, DstDevice TEXT, Protocol INTEGER, SrcPort INTEGER, DstPort INTEGER, SrcPackets INTEGER, DstPackets INTEGER, SrcBytes INTEGER, DstBytes INTEGER) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", to_db)
-c.commit()
-c.close()
+sql = sqlite3.connect('database.db')
+cur = sql.cursor()
+
+cur.execute('''CREATE TABLE IF NOT EXISTS seckin(Time INTEGER, Duration INTEGER, SrcDevice TEXT, DstDevice TEXT, Protocol INTEGER, SrcPort INTEGER, DstPort INTEGER, SrcPackets INTEGER, DstPackets INTEGER, SrcBytes INTEGER, DstBytes INTEGER)''') # create the table if it doesn't already exist
+
+for row in reader:
+	cur.execute("INSERT INTO seckin VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", row)
+
+f.close()
+sql.commit()
+sql.close()
